9b4468eded8a5c352adbc9145bfa606a633241a5
wewantthefunk <wewantthefunk@yahoo.com>
2023-12-07 17:02:14 -0500

    updates

---
+++ hellowor.cbl
@@ -0,0 +1,26 @@
+       ID DIVISION.
+       PROGRAM-ID.   HELLOWO1.
+
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+
+           DISPLAY 'HELLO, WORLD!'.
+
+           DISPLAY 'HI'.
+
+           DISPLAY 'inserted line above'.
+
+           DISPLAY 'HELLO, sir'.
+
+           DISPLAY 'this is a new line'.
+
+           DISPLAY 'GOODBYE'.
+
+      *
+
+      * test for git
+
+           GOBACK.
---
+++ m_hellowor.cbl
@@ -0,0 +1,18 @@
+       ID DIVISION.
+       PROGRAM-ID.   HELLOWO1.
+
+       DATA DIVISION.
+
+       WORKING-STORAGE SECTION.
+
+       PROCEDURE DIVISION.
+
+           DISPLAY 'HELLO, WORLD!'.
+
+           DISPLAY 'HI'.
+
+           DISPLAY 'HELLO'.
+
+           DISPLAY 'GOODBYE'.
+
+           GOBACK.
