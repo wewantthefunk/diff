03476e8d912a2930a295f94dee2c3bc1dd1692ea
wewantthefunk <wewantthefunk@yahoo.com>
2023-12-11 11:15:52 -0500

    updates

--- Utilities.java
+++ Utilities.java
@@ -16,7 +16,7 @@ public class Utilities {
 
     public static String rtrim(String s) {
         if (s.trim().equals(""))
-            return s;
+            return "";
         String n = "_" + s;
         String result = n.trim();
         return result.substring(1);
